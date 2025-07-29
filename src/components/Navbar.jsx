a77dc6cba5d374ee4fca86d25ed188ecec01c73c
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-07-29 22:58:20 +0530

    minor fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,9 +36,9 @@ const Navbar = ({ user, onSignOut, onSearch, cartItemsCount = 0, onCartClick, wi
               {/* <svg className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
               </svg> */}
-              <img src={logo} alt="ShopEase Logo" className="h-20 w-40 object-cover" />
+              <img src={logo} alt="ShopEase Logo" className="h-20 w-40 object-cover cursor-pointer" />
             </div>
-            <h1 className="text-xl font-bold text-gray-900">ShopEase</h1>
+            <h1 className="text-xl font-bold text-gray-900 cursor-pointer">ShopEase</h1>
           </div>
 
           {/* Desktop Navigation */}
